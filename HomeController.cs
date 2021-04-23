a66d91322576ad43aac5eb8b5ff38eccb455d1c9
NicolasGonzA <78570579+NicolasGonzA@users.noreply.github.com>
2021-04-22 23:38:01 -0400

    Add files via upload

---
+++ HomeController.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections.Generic;
+using System.Security.Claims;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Private_API.Controllers
+{
+    public class HomeController : Controller
+
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+
+        [Authorize]
+        public IActionResult Secret()
+        {
+            return View();
+        }
+
+        public IActionResult Authenticate()
+        {
+
+            var NicolasClaims = new List<Claim>()
+            {
+                new Claim(ClaimTypes.Name, "Gonzalez"),
+                new Claim("Nicolas.has", "good grades"),
+            };
+
+            var StudentIDClaims = new List<Claim>()
+            {
+                new Claim(ClaimTypes.Name, "Nicolas Alejandro Gonzalez"),
+                new Claim(ClaimTypes.Email, "200369901.student@georgianc.on.ca"),
+                new Claim("Nicolas.is", "A.Student"),
+            };
+                
+
+
+            var NicolasIdentity = new ClaimsIdentity(NicolasClaims, "Nicolas Identity");
+
+            var StudentIDIdentity = new ClaimsIdentity(StudentIDClaims, "Georgian College");
+
+            var userPrincipal = new ClaimsPrincipal(new[] { NicolasIdentity,StudentIDIdentity });
+
+            HttpContext.SignInAsync(userPrincipal);
+
+            return RedirectToAction("Index");
+
+        }
+    }
+
+
+
+}
---
+++ Startup.cs
@@ -0,0 +1,58 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.EntityFrameworkCore;
+using Private_API.Data;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.AspNetCore.Http;
+
+namespace Private_API
+{
+    public class Startup
+    {
+
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddAuthentication("WebAuth")
+                .AddCookie("WebAuth", config =>
+                {
+                    config.Cookie.Name = "PrivateAPI";
+                    config.LoginPath = "/Home/Authenticate";
+                });
+
+            services.AddControllersWithViews();
+        }
+
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+
+            }
+
+            app.UseRouting();
+            // are you allowed?
+            app.UseAuthentication();
+
+            // who are you?
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+
+            {
+                endpoints.MapDefaultControllerRoute();
+            });
+        }
+    }
+}
